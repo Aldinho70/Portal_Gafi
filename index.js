3e23a0563a6d54fe5eb247327596e2233254f819
Jesus Manuel Bobadilla Villagrana <jesus.bobadilla@metricamovil.com>
2025-09-03 09:40:13 -0600

    [Version con informacion de unidad] - Se vinxulo el desarrollo de la seccion de detalles de unidad - Se agrega el iframe del desarrollo de gafi original

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { TOKEN } from "./config.js";
 import wialonSDK from "./src/API/wialon/sdk/wialonSDK.js";
 import { Footer } from "./src/componets/Footer/Footer.js";
 import { createSectionsUnits} from "./src/componets/Units/Units.js";
-import { unitSectionDetails } from "./src/componets/Units/UnitsSecion.js";
+import { unitSectionDetails, createSectionDetailsUnit } from "./src/componets/Units/UnitsSecion.js";
 import { getLocationsOfUnits, colapseSectionUnit  } from "./src/utils/utils.js";
-import { initMap, flyCameraToByUnit, resetMap } from "./src/API/google_maps/google_maps.js";
+import { initMap, flyCameraToByUnit } from "./src/API/google_maps/google_maps.js";
 
 window.flyCameraToByUnit = flyCameraToByUnit;
 window.unitSectionDetails = unitSectionDetails;
@@ -22,16 +22,7 @@ async function iniciarWialon(token) {
   
   $("body").append( google_maps );
   $("body").append( createSectionsUnits( units ) );
-  $("body").append( `<div id="right-panel" class="closed">
-        <div class="panel-header">
-            <h3>Información Adicional</h3>
-            <button id="close-right-panel-btn" onClick="(colapseSectionUnit())"class="btn btn-sm btn-light">Cerrar</button>
-        </div>
-        <div class="panel-content">
-            <p>Contenido del panel derecho que se puede ocultar.</p>
-            <p>Puedes agregar aquí detalles específicos del vehículo, gráficos o más controles.</p>
-        </div>
-    </div>` );
+  $("body").append( createSectionDetailsUnit() );
   $("body").append( Footer() );
 
 }
--- src/componets/Units/UnitsSecion.js
+++ src/componets/Units/UnitsSecion.js
@@ -1,5 +1,28 @@
 export const unitSectionDetails = ( unit ) => {
     console.log(unit);
-    
-     $("#right-panel").removeClass("closed");
+    $("#iframeDetailsUnit").attr("src", `http://ws4cjdg.com/RendimientoGafi.com/index.html?idUnit=${unit}`);
+    $("#right-panel").removeClass("closed");
+
 }
+
+export const createSectionDetailsUnit = (  ) => {
+    return `<div id="right-panel" class="closed">
+        <div class="panel-header">
+            <h3>Información de consumos de combustible de 15 dias</h3>
+            <button id="close-right-panel-btn" onClick="(colapseSectionUnit())"class="btn btn-sm btn-danger">Cerrar</button>
+        </div>
+        <div class="panel-content" style="width: 100%;height: 98%;">
+          <iframe
+            id="iframeDetailsUnit"
+            title="Inline Frame Example"
+            width="100%"
+            height="100%"
+            src="http://127.0.0.1:5501/index.html">
+          </iframe>
+        </div>
+    </div>`
+}
+
+/* http://127.0.0.1:5501/index.html?idUnit=28776132&name=Gafi-516 */
+/* http://ws4cjdg.com/RendimientoGafi.com/ */
+/* http://ws4cjdg.com/RendimientoGafi.com/index.html?idUnit=28776132 */
